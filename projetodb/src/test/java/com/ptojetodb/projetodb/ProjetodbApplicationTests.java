3563f4c50d99f751f99d23c6b7f11f5abb7c1b1c
Danny Marques <daninhacmmarques@gmail.com>
2024-12-07 22:22:13 -0300

    feat: configura estrutura inicial do projeto
    Co-authored-by:Sylvia <sylviavitoriasilva@gmail.com>

---
+++ projetodb/src/test/java/com/ptojetodb/projetodb/ProjetodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ptojetodb.projetodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
