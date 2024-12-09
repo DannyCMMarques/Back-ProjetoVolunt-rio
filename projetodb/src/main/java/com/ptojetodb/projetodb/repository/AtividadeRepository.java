529c424efa113fb49f7f335c239f9c551869f495
sylviavitoria <sylviavitoriasilva@gmail.com>
2024-12-09 16:59:58 -0300

    #5 Criação UsuarioRepository

--- projetodb/src/main/java/com/ptojetodb/projetodb/repository/AtividadeRepository.java
+++ projetodb/src/main/java/com/ptojetodb/projetodb/repository/AtividadeRepository.java
@@ -1,11 +1,10 @@
 package com.ptojetodb.projetodb.repository;
 
-import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ptojetodb.projetodb.model.Atividade;
 
-public interface AtividadeRepository extends JpaRepository<Atividade, UUID> {
+public interface AtividadeRepository extends JpaRepository<Atividade, Long> {
 
 }
