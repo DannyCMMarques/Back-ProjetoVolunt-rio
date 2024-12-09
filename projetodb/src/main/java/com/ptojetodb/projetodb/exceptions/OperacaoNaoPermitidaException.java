65e17187b9bf3ddfd4e29c130d38c8985555a480
Danny Marques <daninhacmmarques@gmail.com>
2024-12-09 18:09:52 -0300

    feat: criacao do AtividadeService, atualização do atividadeRepository e exceptions

---
+++ projetodb/src/main/java/com/ptojetodb/projetodb/exceptions/OperacaoNaoPermitidaException.java
@@ -0,0 +1,7 @@
+package com.ptojetodb.projetodb.exceptions;
+
+public class OperacaoNaoPermitidaException extends RuntimeException {
+    public OperacaoNaoPermitidaException(String message) {
+        super(message);
+    }
+}
