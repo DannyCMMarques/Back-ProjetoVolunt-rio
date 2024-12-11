95d7e61c0d0ac43ba5d2f5d483cbde28eb7e9610
sylviavitoria <sylviavitoriasilva@gmail.com>
2024-12-11 14:30:42 -0300

    feat: Criação do UsuarioDTO utilizando Mappers e upadate de Usuarios

--- projetodb/src/main/java/com/ptojetodb/projetodb/controller/dto/UsuarioDTO.java
+++ projetodb/src/main/java/com/ptojetodb/projetodb/controller/dto/UsuarioDTO.java
@@ -4,6 +4,8 @@ import java.time.LocalDate;
 import jakarta.validation.constraints.NotBlank;
 
 public record UsuarioDTO(
+
+    Long idUsuario,
     @NotBlank(message = "campo obrigatorio")
     String nome,
 
