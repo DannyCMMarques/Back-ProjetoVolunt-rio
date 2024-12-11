e1c79ca3fb251da66bd5dfbe37d0fe11850bb407
Danny Marques <daninhacmmarques@gmail.com>
2024-12-10 22:52:52 -0300

    feat: criação do auth controller para possibilitar o login

--- projetodb/src/main/java/com/ptojetodb/projetodb/repository/UsuarioRepository.java
+++ projetodb/src/main/java/com/ptojetodb/projetodb/repository/UsuarioRepository.java
@@ -1,7 +1,12 @@
 package com.ptojetodb.projetodb.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.ptojetodb.projetodb.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    public Optional<Usuario> findByEmail(String email);
 }
