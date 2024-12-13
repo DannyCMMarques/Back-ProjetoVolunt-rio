a7d394fa11f2bc3045a21e0319d1b2097fea1db5
Danny Marques <daninhacmmarques@gmail.com>
2024-12-13 19:07:50 -0300

    fix: adição de @service em userService

--- projetodb/src/main/java/com/ptojetodb/projetodb/security/UserService.java
+++ projetodb/src/main/java/com/ptojetodb/projetodb/security/UserService.java
@@ -3,7 +3,9 @@ package com.ptojetodb.projetodb.security;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService{
 
     public  Long getAuthenticatedUserId() {
